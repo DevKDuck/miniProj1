d3d4686a36d0b0cfac163b0720d3ebd8d5ab48ae
DevKDuck <loading95@naver.com>
2024-03-27 18:59:27 +0900

    MemberVO필드명 변경

--- src/main/java/member/MemberVO.java
+++ src/main/java/member/MemberVO.java
@@ -10,11 +10,29 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 public class MemberVO {
-	private String memberid;
-	private String memberpwd;
-	private String membername;
-	private String memberaddress;
-	private String memberphonenumber;
-	private String membergender;
+	private String member_id;
+	private String member_pwd;
+	private String member_name;
+	private String member_address;
+	private String member_phonenumber;
+	private String member_gender;
+	
+
+	//실행 명령키
+	private String action;
+	
+	public MemberVO(String member_id, String member_pwd, String member_name, String member_address,
+			String member_phonenumber, String member_gender) {
+		super();
+		this.member_id = member_id;
+		this.member_pwd = member_pwd;
+		this.member_name = member_name;
+		this.member_address = member_address;
+		this.member_phonenumber = member_phonenumber;
+		this.member_gender = member_gender;
+	}
+
+
+
 
 }
